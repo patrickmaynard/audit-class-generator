077ab65ea8697fe70297eb1652f0f23077468d1e
Steffen Grahl <steffen@sgwebservice.de>
2026-02-18 18:01:59 +0100

    feat: Make audit-class-generator configurable

--- Applier.php
+++ Applier.php
@@ -4,43 +4,14 @@ namespace PatrickMaynard\AuditClassGenerator;
 
 class Applier
 {
-    public function getDirContents($dir){
-        $results = array();
-        $files = scandir($dir);
+    private Config $config;
 
-            foreach($files as $key => $value){
-                    if(!is_dir($dir. DIRECTORY_SEPARATOR .$value)){
-                        $results[] = $dir. DIRECTORY_SEPARATOR . $value;
-                    } else if(
-                        is_dir($dir. DIRECTORY_SEPARATOR .$value) &&
-                        !str_contains($value, '..') &&
-                        $value !== '.'
-                    ) {
-                        $results[] = $value;
-                        $results = array_merge($results, $this->getDirContents($dir. DIRECTORY_SEPARATOR .$value));
-                    }
-            }
-            return $results;
-
-    }
-
-    //TODO: Allow blade as well.
-    public function filterAllFilesForTwigExtension()
+    public function __construct(Config $config)
     {
-        $directoryToParse = getcwd() . '..' . DIRECTORY_SEPARATOR . ' .. ';
-        echo PHP_EOL . 'We will be looking recursively in ' . $directoryToParse . ' for Twig files .. ' . PHP_EOL;
-        $files = $this->getDirContents($directoryToParse);
-        $output = [];
-        foreach ($files as $file) {
-            if (str_ends_with($file, '.html.twig')) {
-                $output[] = $file;
-            }
-        }
-        return $output;
+        $this->config = $config;
     }
 
-
-    public function applyAllAuditTags()
+    public function applyAllAuditTags(): void
     {
         //First, we need to be on a safe git branch, since this set of changes will mess with lots of files
         exec('git checkout -b temporary-template-audit-branch-remove-me-soon');
@@ -54,28 +25,73 @@ class Applier
         foreach ($files as $index => $file) {
             $contents = file_get_contents($file);
             preg_match_all('/class="[^"]+"/', $contents, $matches);
+
             foreach ($matches as $index => $match) {
-                if ( $index = 0 ) {
+                if ($index = 0) {
                     continue;
                 }
 
                 if (is_array($match)) {
+
                     foreach ($match as $matchingString) {
-                        $hash = md5(rand(0,1000000000) . "This will provide a nice, long, randomish string.");
+                        $hash = md5(rand(0, 1000000000) . "This will provide a nice, long, randomish string.");
                         $shorterFingerprint = 'audit_' . substr($hash, 0, 6);
                         $withSpace = ' ' . $shorterFingerprint . ' ';
 
                         //Now just do a simple string replacement
                         $contents = str_replace(
-                            $matchingString, substr($matchingString, 0, -1) . $withSpace . '"', $contents
+                            $matchingString,
+                            substr($matchingString, 0, -1) . $withSpace . '"',
+                            $contents,
                         );
                     }
                 }
-
             }
+
             file_put_contents($file, $contents);
         }
+
         echo "Done!";
         echo PHP_EOL;
+    }
+
+    //TODO: Allow blade as well. Maybe solved with configuration
+    private function filterAllFilesForTwigExtension(): array
+    {
+        $directoryToParse = getcwd() . DIRECTORY_SEPARATOR  . $this->config->getDirectory();
+
+        echo PHP_EOL . 'We will be looking recursively in ' . $directoryToParse . ' for Twig files .. ' . PHP_EOL;
+
+        $files = $this->getDirContents($directoryToParse);
+        $output = [];
+
+        foreach ($files as $file) {
+            if (str_ends_with($file, $this->config->getExtension())) {
+                $output[] = $file;
+            }
+        }
+
+        return $output;
+    }
+
+    private function getDirContents($dir): array
+    {
+        $results = [];
+        $files = scandir($dir);
+
+        foreach ($files as $key => $value) {
+            if (!is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+                $results[] = $dir . DIRECTORY_SEPARATOR . $value;
+            } elseif (
+                is_dir($dir . DIRECTORY_SEPARATOR . $value)
+                && !str_contains($value, '..')
+                && $value !== '.'
+            ) {
+                $results[] = $value;
+                $results = array_merge($results, $this->getDirContents($dir . DIRECTORY_SEPARATOR . $value));
+            }
+        }
+
+        return $results;
     }
 }
---
+++ Config.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types = 1);
+
+namespace PatrickMaynard\AuditClassGenerator;
+
+class Config {
+    private string $directory;
+    private string $extension;
+    private bool $verbose;
+
+    private function __construct(
+        string $directory,
+        string $extension,
+        bool $verbose
+    ) {
+        $this->directory = $directory;
+        $this->extension = $extension;
+        $this->verbose = $verbose;
+    }
+
+    public static function create(array $options): self
+    {
+        return new self(
+            $options['directory'] ?? $options['d'] ?? 'templates',
+            $options['extension'] ?? $options['e'] ?? '.html.twig',
+            isset($options['v']) || isset($options['verbose'])
+        );
+    }
+
+    public function getDirectory(): string
+    {
+        return $this->directory;
+    }
+
+    public function getExtension(): string
+    {
+        return $this->extension;
+    }
+
+    public function isVerbose(): bool
+    {
+        return $this->verbose;
+    }
+}
